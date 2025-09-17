172e12e7f09e15ccbe5ae67aa15bd3b4949c70f6
comaychiendau <a1902421@adelaide.edu.au>
2025-09-17 15:00:59 +0930

    Update on randmmu.py

--- randmmu.py
+++ randmmu.py
@@ -1,34 +1,82 @@
+import random
 from mmu import MMU
 
 class RandMMU(MMU):
     def __init__(self, frames):
-        # TODO: Constructor logic for RandMMU
-        pass
+        self.num_frames = frames
+        self.frames = {} 
+        self.ptable = {} 
+        self.disk_reads = 0
+        self.disk_writes = 0
+        self.page_faults = 0
+        self.debug = False
 
     def set_debug(self):
-        # TODO: Implement the method to set debug mode
-        pass
+        self.debug = True
 
     def reset_debug(self):
-        # TODO: Implement the method to reset debug mode
-        pass
+        self.debug = False
 
     def read_memory(self, page_number):
-        # TODO: Implement the method to read memory
-        pass
+        if page_number in self.ptable:
+            if self.debug == True:
+                print(f"hit: page ({page_number}). frame = {self.ptable[page_number]}")
+            return  # hit
+        else:
+            # Page fault
+            self.page_faults += 1
+            self.disk_reads += 1
+            if len(self.frames) < self.num_frames:
+                # Free frame available
+                frame_index = len(self.frames)
+                self.frames[frame_index] = page_number
+                self.ptable[page_number] = frame_index
+                if self.debug:
+                    print(f"fault: load {page_number} into frame {frame_index}")
+            else:
+                #find a frame to become sacrificial object
+                sheep = random.choice(list(self.frames.keys()))
+                sheepage = self.frames[sheep]
+
+
+                self.frames[sheep] = page_number
+                self.ptable.pop(sheepage)
+                self.ptable[page_number] = sheep
+
+                if self.debug == True:
+                    print(f"read fault: replaced {sheepage} with {page_number} in frame {sheep}")
 
     def write_memory(self, page_number):
-        # TODO: Implement the method to write memory
-        pass
+        if page_number in self.ptable:
+            if self.debug == True:
+                print(f"WRITE HIT: page {page_number} in frame {self.ptable[page_number]}")
+        else:
+            self.page_faults += 1
+            self.disk_reads += 1
+            if len(self.frames) < self.num_frames:
+                frame_index = len(self.frames)
+                self.frames[frame_index] = page_number
+                self.ptable[page_number] = frame_index
+                if self.debug:
+                    print(f"WRITE FAULT: loaded page {page_number} into free frame {frame_index}")
+            else:
+                sheep = random.choice(list(self.frames.keys()))
+                sheepage = self.frames[sheep]
+
+                self.disk_writes += 1
+
+                self.frames[sheep] = page_number
+                self.ptable.pop(sheepage)
+                self.ptable[page_number] = sheep
+
+                if self.debug == True:
+                    print(f"WRITE FAULT: replaced page {sheepage} with {page_number} in frame {sheep}")
 
     def get_total_disk_reads(self):
-        # TODO: Implement the method to get total disk reads
-        return -1
+        return self.disk_reads
 
     def get_total_disk_writes(self):
-        # TODO: Implement the method to get total disk writes
-        return -1
+        return self.disk_writes
 
     def get_total_page_faults(self):
-        # TODO: Implement the method to get total page faults
-        return -1
+        return self.page_faults
